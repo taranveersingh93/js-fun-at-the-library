512e11dbda02883113954383bdf8139bae8b98ef
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-22 21:22:45 -0400

    Complete the book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var ghoulCharacter = {};
+  ghoulCharacter.name = name;
+  ghoulCharacter.age = age;
+  ghoulCharacter.pronouns = pronouns;
+  return ghoulCharacter;
+}
+
+function saveReview(input, inputArray) {
+  if(inputArray.includes(input)===false) {
+    inputArray.push(input);
+  }
+  return inputArray;
+}
+
+function calculatePageCount(bookTitle) {
+  return (20*bookTitle.length); 
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {};
+  book.title = bookTitle;
+  book.mainCharacter = bookCharacter;
+  book.pageCount = calculatePageCount(bookTitle);
+  book.genre = genre;
+  return book;
+}
+
+function editBook(bookObject) {
+  bookObject.pageCount = bookObject.pageCount*3/4;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
