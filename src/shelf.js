6f9b488d6081934f2d79cbc3b0c8212d3fc68e02
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-23 21:07:32 -0400

    Solve all shelf questions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
 
+function shelfBook (book, shelf) {
+  if(shelf.length<3) {
+    shelf.unshift(book);
+  }
+  return shelf;
+}
+
+function unshelfBook(book, shelf) {
+  for(var i=0; i<shelf.length; i++) {
+    if(shelf[i].title===book) {
+      console.log("condition met");
+      shelf.splice(i,1);
+    }
+  }
+  return shelf;
+}
+
+function listTitles (shelf) {
+  var titles = [];
+  for(var i=0; i<shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(", "); 
+}
+
+function searchShelf (shelf, bookTitle) {
+  var isShelved = false; //initializing this as false unless proven otherwise.
+  for(var i=0; i<shelf.length; i++) {
+    if(shelf[i].title === bookTitle) {
+      isShelved = true;
+      break;
+    }
+  }
+  return isShelved;
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
