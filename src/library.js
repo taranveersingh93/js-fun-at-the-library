d87cb4a19c7d528545971f3fd0abf94516a72e5d
Taranveer Singh <taranveersingh93@gmail.com>
2023-03-24 16:34:52 -0400

    Refactor library for readability

--- src/library.js
+++ src/library.js
@@ -10,16 +10,16 @@ function createLibrary(inputName) {
 
 function addBook(library, book) {
   var bookGenre = book.genre;
-  library["shelves"][bookGenre].push(book);
+  library["shelves"][bookGenre].push(book); //add book to book's genre shelf
   return library;
 }
 
 function checkoutBook(library, bookTitle, bookGenre) {
   var bookExists = false;
-  for(var i=0; i<library.shelves[bookGenre].length; i++) {
+  for(var i=0; i<library["shelves"][bookGenre].length; i++) { //check if book exists
     if(library["shelves"][bookGenre][i].title===bookTitle) 
-    library["shelves"][bookGenre].splice(i,1);
-    bookExists = true;
+    library["shelves"][bookGenre].splice(i,1); //alter the shelf data 
+    bookExists = true; // with this established, proceed to the two possible returns.
   } 
  if(bookExists === true) {
   return `You have now checked out ${bookTitle} from the ${library.name}.`; 
@@ -30,12 +30,14 @@ function checkoutBook(library, bookTitle, bookGenre) {
 }
 
 function takeStock(library, bookGenre) {
+var specificShelf = library["shelves"][bookGenre]; //this is an array
+var listAllShelves = Object.keys(library.shelves); 
 
   if(arguments.length===2) {
-    return `There are a total of ${library.shelves[bookGenre].length} ${bookGenre} books at the ${library.name}.`;
+    return `There are a total of ${specificShelf.length} ${bookGenre} books at the ${library.name}.`;
   } else {
     var libraryCount = 0;
-    for(var i=0; i<Object.keys(library.shelves).length; i++) {
+    for(var i=0; i<listAllShelves.length; i++) {
       libraryCount = libraryCount + Object.values(library.shelves)[i].length;
     }
     return `There are a total of ${libraryCount} books at the ${library.name}.`
